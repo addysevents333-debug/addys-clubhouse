2584152191a24755a9bd176fd99987f0abc850b4
addysevents333-debug <addysevents333@gmail.com>
2026-06-01 13:57:52 -0400

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -507,6 +507,20 @@ const deletePost = async (id) => {
   setMessage("Please add a note title and content.");
   return;
 }
+    const deleteNote = async (id) => {
+  const { error } = await supabase
+    .from("notes")
+    .delete()
+    .eq("id", id);
+
+  if (!error) {
+    setNoteMessage("Note deleted.");
+    loadNotes();
+  } else {
+    console.log(error);
+    setNoteMessage(error.message || "Error deleting note.");
+  }
+};
 
   const { error } = await supabase
     .from("notes")
