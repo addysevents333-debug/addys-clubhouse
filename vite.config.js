c2a21c93c0dd03ed3df8bf2cc3f3268db1b047b1
addysevents333-debug <addysevents333@gmail.com>
2026-06-12 12:41:42 -0400

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "icon-192.png",
+        "icon-512.png",
+      ],
+      manifest: false,
+      workbox: {
+        navigateFallback: "/index.html",
+        globPatterns: [
+          "**/*.{js,css,html,png,jpg,jpeg,svg,webp,ico}",
+        ],
+      },
+    }),
+  ],
+});
