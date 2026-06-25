7c644cf94fbd332b9e954a362504bb964529160f
addysevents333-debug <addysevents333@gmail.com>
2026-06-25 11:29:09 -0400

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -975,7 +975,6 @@ const deleteOffer = async (id) => {
     ]);
 
   if (!error) {
-    alert("About to call send-push");
     const { data: pushData, error: pushError } =
   await supabase.functions.invoke("send-push", {
     body: {
@@ -984,7 +983,7 @@ const deleteOffer = async (id) => {
       category: pushCategory,
     },
   });
-alert("Finished calling send-push");
+
 console.log("Push result:", pushData, pushError);
 
 if (pushError) {
